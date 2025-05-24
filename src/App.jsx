8ef39bbbdef30c498fffb2d14522dbbb883e395d
Ogzhn1616 <ousenpai@gmail.com>
2025-05-24 21:19:33 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="bg-gradient-to-b from-pink-100 via-yellow-100 to-white text-red-500 font-press min-h-screen flex flex-col justify-center items-center text-center px-4">
+      <h1 className="text-3xl mb-6 typewriter">Hazır mısın Onur?</h1>
+      <p className="text-xl fade-in">
+        Bu küçük oyunu sadece senin için yaptım 💛
+      </p>
+    </div>
+  )
+}
+
+export default App
